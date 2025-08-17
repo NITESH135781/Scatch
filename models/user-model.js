2f9bb055d83689ee7a4d06fdd92f71c445622de0
NITESH135781 <niteshkushram982@gmail.com>
2025-08-18 03:25:47 +0530

    collection created

--- models/user-model.js
+++ models/user-model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password: String,
     cart: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'products'
+        ref: 'product'
     }],
     orders:{
         type: Array,
